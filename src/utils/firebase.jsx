67a5041b374f21c7514e45dac5445b243831188a
lakshmitanujakota <160825767+lakshmitanujakota@users.noreply.github.com>
2026-01-05 20:08:17 +0530

    Update firebase.jsx

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB2pi7QH43i9XgAGgfv1zpk1Yy85l4P4XU",
-  authDomain: "netflixgpt-a7789.firebaseapp.com",
-  projectId: "netflixgpt-a7789",
-  storageBucket: "netflixgpt-a7789.firebasestorage.app",
-  messagingSenderId: "751683706102",
-  appId: "1:751683706102:web:fa4254c7435ad33c1fe0d7"
+  apiKey: "***************",
+  authDomain: "*************************",
+  projectId: "************************",
+  storageBucket: "*********************************",
+  messagingSenderId: "***********************",
+  appId: "****************************************"
 };
 
 // Initialize Firebase
